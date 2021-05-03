5772c144ed982f935f0f908e5c94bcae7819f6be
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-05-03 18:13:43 +0530

    changes to reqs

--- apiBase.py
+++ apiBase.py
@@ -24,7 +24,7 @@ def home():
 
 @app.route('/checkSet', methods=['GET'])
 def getDataBases():
-    return jsonify(mdb.getUser())
+    return str(mdb.getUser())
 
 # search players information
 @app.route('/pId/<unique_id>')
