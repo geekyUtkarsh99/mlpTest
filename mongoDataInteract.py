46e43f657f4880c847ad9510265f9c61f71ab9ca
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-05-03 18:40:05 +0530

    changes to reqs

--- mongoDataInteract.py
+++ mongoDataInteract.py
@@ -13,7 +13,7 @@ def checkDataBaseExistance(db):
 def getUser():
     users = database['users']
     data = []
-    cols = users.find({"pid1"})
+    cols = users.find({"_id":0})
     i = 0
     for u in cols:
         data.append(u)
