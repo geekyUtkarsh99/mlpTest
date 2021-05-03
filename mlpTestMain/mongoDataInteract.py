7dd10d03923596ab8016e723eca0e0b40c48a1ad
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-05-03 16:22:54 +0530

    commit

---
+++ mlpTestMain/mongoDataInteract.py
@@ -0,0 +1,8 @@
+import pymongo as mongodb
+
+client = mongodb.MongoClient("mongodb+srv://utkarsh:<utkarsh99>@cluster0.hkw3v.mongodb.net/test")
+
+database = client["players"]
+
+def checkDataBaseExistance(database):
+    return client.list_database_names()
