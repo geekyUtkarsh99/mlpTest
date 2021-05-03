ccc0287a76474723ae9cb1a1f2e6edd8d17a2068
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-05-03 17:25:46 +0530

    changes to reqs

--- mongoDataInteract.py
+++ mongoDataInteract.py
@@ -2,9 +2,9 @@ import pymongo as mongodb
 
 client = mongodb.MongoClient("mongodb+srv://geekyUtkarsh99:utkarsh99@cluster0.hkw3v.mongodb.net/test")
 
-database= client['players']
+database= client.get_database('players')
 
 
 def checkDataBaseExistance(db):
-    return database
+    return database.list_collection_names()
 
