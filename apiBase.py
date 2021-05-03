29a8a70da6c05ad2973098e6da9f1ab736b21b82
utkarsh choudhary <utkarshchoudhary80@gmail.com>
2021-05-03 16:34:47 +0530

    changes to reqs

---
+++ apiBase.py
@@ -0,0 +1,71 @@
+from flask import Flask as fsk
+import dataHandle as dh
+import threading
+from flask import jsonify
+import mongoDataInteract as mdb
+import json
+
+app = fsk(__name__)
+app.config["DEBUG"] = True
+
+pname = 'abc'
+
+roomList = json.load(open('rooms.json'))
+
+# rList = roomList["rooms"]
+
+player = json.load(open('players.json'))
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return 'initialize home'
+
+
+@app.route('/checkSet', methods=['GET'])
+def getDataBases():
+    return str(mdb.checkDataBaseExistance(None))
+
+
+# search players information
+@app.route('/pId/<unique_id>')
+def getUser(unique_id):
+    return jsonify(player[unique_id])
+
+
+@app.route('/gameplay/<room_id>')
+def showRoom(room_id):  # access room
+    return jsonify(roomList[room_id])
+
+
+@app.route('/gameplay/search/<player_id>')
+def startSearch(player_id):  # get room information
+    return jsonify(dh.addActivePlayers(player_id))
+
+
+@app.route('/register/<email>')
+def register_new(email):  # register new users
+    new_player = {
+        email: {
+            "x": 0.0,
+            "y": 0.0
+        }
+    }
+    check_duplicate = check_redundancy(email)
+    if check_duplicate:
+        player.update(new_player)
+        with open('players.json', 'w') as file:
+            json.dump(player, file)
+        return "success"
+    return "failed to create id"
+
+
+def check_redundancy(email):
+    for i in player:
+        if email == i:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    app.run(port=8081, host='0.0.0.0')
